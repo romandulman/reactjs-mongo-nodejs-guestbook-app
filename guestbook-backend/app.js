787b11c9b03ae35af3cebc2fad9d8caa0205eb67
Roman Dulman <47609139+romandulman@users.noreply.github.com>
2019-05-29 21:06:23 +0300

    Added code

--- guestbook-backend/app.js
+++ guestbook-backend/app.js
@@ -1,16 +1,12 @@
-
-
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var createError = require('http-errors');
-var logger = require('morgan');
-var path = require('path');
-var cors = require('cors');
-var mongoose = require('mongoose');
-var app = express();
+let express = require('express');
+let cookieParser = require('cookie-parser');
+let createError = require('http-errors');
+let logger = require('morgan');
+let path = require('path');
+let cors = require('cors');
+let mongoose = require('mongoose');
+let app = express();
 app.use(cors());
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -19,23 +15,22 @@ app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true}); // dev mongo container
 // //mongoose.connect('mongodb://mongodb:27017/testdb', {useNewUrlParser: true}); //production docker network
 
-var userSchema = mongoose.Schema({
+let userSchema = mongoose.Schema({
     Name: String,
     Body: String
 });
 
-var db = mongoose.connection;
-var collection = db.collection('testcoll');
+let db = mongoose.connection;
+let collection = db.collection('testcoll');
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
+db.once('open', () => {
     console.log('Mongo Connected');
-    collection.find({Name: "Avi"}).toArray(function (err, guests) {
+    collection.find({Name: "Avi"}).toArray((err, guests) => {
         console.log(guests[0].Body) // test it
     });
 
 });
-
 
 app.get('/guests', (req, res) => {
     collection.find().toArray(function (err, guests) {
@@ -50,10 +45,9 @@ app.post('/postguest', (req, res) => {
             Name: req.body.arr.Name,
             Body: req.body.arr.Body
         }
-    )
+    );
     res.send('ok');
 });
-
 
 app.delete('/deleteguest', (req, res) => {
 
@@ -61,13 +55,12 @@ app.delete('/deleteguest', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     next(createError(404));
 });
 
-
 // // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
 //   // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -77,9 +70,8 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-var server = app.listen(8080, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
+let server = app.listen(8080,() =>{
+    let host = server.address().address;
+    let port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port)
 });
