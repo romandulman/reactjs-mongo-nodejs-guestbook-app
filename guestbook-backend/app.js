db04f2e648557505345227aa3d46d9b4d46c4f84
Roman Dulman <romandulman@gmail.com>
2019-06-01 22:45:13 +0300

    Added code

--- guestbook-backend/app.js
+++ guestbook-backend/app.js
@@ -77,7 +77,10 @@ app.use((err, req, res, next) => {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.status(500).json({
+        message: err.message,
+        error: err
+    });
 });
 
 let server = app.listen(8080, () => {
