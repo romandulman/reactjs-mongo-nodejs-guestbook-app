04c25d99d1cbdf610c4801091852e1818b749a65
Roman Dulman <romandulman@gmail.com>
2019-07-15 22:37:26 +0300

    added new code

---
+++ guestbook-backend/config/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    google: {
+        clientID: '408762446780-bbe353b9unu99l2td3rnknn7uf367ek9.apps.googleusercontent.com',
+        clientSecret: 'q46anuHqmHTCARt7nQ9SIMLY'
+    },
+
+    authMongoDB: {
+        dbURL: 'mongodb://192.168.2.14:27017/testdb',
+        useNewUrlParser: true
+    },
+     session: {
+        cookie_key: "ssshhhh"
+    }
+}
