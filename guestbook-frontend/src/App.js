ef3d63f0bf13b62496f5b8aa062da3f6bfd3b5cb
Roman Dulman <romandulman@gmail.com>
2019-07-15 22:49:21 +0300

    added new code

--- guestbook-frontend/src/App.js
+++ guestbook-frontend/src/App.js
@@ -4,7 +4,7 @@ import Footer from './Components/Footer'
 import GuestBook from './Components/GuestBook'
 import Profile from './Components/Profile'
 import Header from './Components/Header'
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route,Redirect, Link} from "react-router-dom";
 
 
 class App extends Component {
@@ -14,7 +14,9 @@ class App extends Component {
             <div className="App">
                 <Router>
                     <Header/>
-
+                    <Route path='/'>
+                        <Redirect to="/guests" />
+                    </Route>
                     <Route path="/profile" component={Profile}/>
                     <Route path="/guests" component={GuestBook}/>
 
