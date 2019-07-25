5557f316b2fe1c589d12fb4386592196831a78e5
Roman <romandulman@gmail.com>
2019-07-26 00:48:07 +0300

    added new code

--- guestbook-frontend/selenium-test/urls_.js
+++ guestbook-frontend/selenium-test/urls_.js
@@ -4,6 +4,6 @@ module.exports = {
   },
   appTestServer: {
     appContainerInstanceLocal: "http://your-local-machine-ip:3000", //not mandatory, i using this for local testing before i run all CD/CI process
-    appContainerInstance: "http://your-app-stating-server:8080" //  deployed within the CD/CI process
+    appContainerInstance: "http://your-app-testing-server:8080" //  deployed within the CD/CI process
   }
 };
