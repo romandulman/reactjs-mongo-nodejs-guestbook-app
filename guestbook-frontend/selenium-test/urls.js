e9877738c458cbf4b2bd8bb6b60932ba19b68c99
Roman <romandulman@gmail.com>
2019-07-29 21:06:42 +0300

    added new code

---
+++ guestbook-frontend/selenium-test/urls.js
@@ -0,0 +1,9 @@
+module.exports = {
+  QATestServer: {
+    seleniumGridHub: "http://192.168.2.12:4444/wd/hub"
+  },
+  appTestServer: {
+    appContainerInstanceLocal: "http://192.168.1.13:3000",
+    appContainerInstance: "http://192.168.2.15:8080"
+  }
+};
