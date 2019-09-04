7983b4b73e73ef09ce91dc965a3afb28c7cb7cd7
Roman <romandulman@gmail.com>
2019-09-04 23:01:31 +0300

    added new code

--- guestbook-backend/controllers/guestCtl.js
+++ guestbook-backend/controllers/guestCtl.js
@@ -7,28 +7,28 @@ const cryptoRandomString = require("crypto-random-string");
 class guestCtl {
   addGuest(req, res) {
 
-    if (req.body.arr.Image.length > 2){
+    if (req.body.arr.Image.length > 2) {
       const base64String = req.body.arr.Image;
-      const base64Image = base64String.split(';base64,').pop();
+      const base64Image = base64String.split(";base64,").pop();
       const buf = Buffer.from(base64Image, "base64");
       const filename = cryptoRandomString({ length: 10, type: "url-safe" });
       var imageUrl = "/image_uploads/" + filename + ".png";
       fs.writeFile(
-          path.join(__dirname, "../public/image_uploads", filename + ".png"), buf,
-          (error)=> {
-            if (error) {
-              throw error;
-            } else {
-              console.log("File created from base64 string");
-              console.log("image URL:"+ imageUrl);
-              return true;
-            }
+        path.join(__dirname, "../public/image_uploads", filename + ".png"),
+        buf,
+        error => {
+          if (error) {
+            throw error;
+          } else {
+            console.log("File created from base64 string");
+            console.log("image URL:" + imageUrl);
+            return true;
           }
+        }
       );
-    }else{
-      imageUrl = "/image_uploads/" + 'default-guest' + ".png";
+    } else {
+      imageUrl = "/image_uploads/" + "default-guest" + ".png";
     }
-
     new Guest({
       Name: req.body.arr.Name,
       Body: req.body.arr.Body,
@@ -40,38 +40,32 @@ class guestCtl {
         console.log(newGuest);
       });
   }
-  decode_base64(base64str) {
-    let buf = Buffer.from(base64str, "base64");
-    let filename = cryptoRandomString({ length: 10, type: "base64" });
-    fs.writeFile(
-        path.join(__dirname, "../public/image_uploads", filename + ".png"),
-        buf,
-        function(error) {
-          if (error) {
-            throw error;
-          } else {
-            console.log("File created from base64 string!");
-            return true;
-          }
-        }
-    );
-  }
 
-  loadAllGuets(req, res) {
+
+  getAllGuets = (req, res) => {
     Guest.find({}).then(AllGuests => {
       if (AllGuests) {
         res.send(Object.values(AllGuests));
       }
     });
-  }
+  };
 
-  deleteGuest(guestid, req, res) {
+  deleteGuest = (req, res) => {
+    const GuestId = req.params.id;
     Guest.deleteOne({
-      _id: guestid
+      _id: GuestId
     }).then(deleted => {
       res.send(deleted);
     });
-  }
+  };
+
+  getSingleGuest = (req, res) => {
+    Guest.find({
+      Name: req.params.name
+    }).then(specGuest => {
+      res.send(specGuest[0]._id);
+    });
+  };
 }
 
 module.exports = new guestCtl();
